cd709c836e67e4d6a1532e6b53224719f445c039
skrypka-vladyslav <33196772+skrypka-vladyslav@users.noreply.github.com>
2025-02-07 19:22:30 +0200

    Update LoginView.swift

--- LoginView.swift
+++ LoginView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+@available(macOS 11.0, *)  // Указываем, что поддерживаем только macOS 11.0 и выше
 struct LoginView: View {
     @State private var emailOrPhone = ""
     @State private var isLoggedIn = false
