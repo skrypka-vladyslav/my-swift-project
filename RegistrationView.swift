6f280a15eefb0517c55ddcade2aaf8fc0e2f3106
skrypka-vladyslav <33196772+skrypka-vladyslav@users.noreply.github.com>
2025-02-07 17:52:39 +0200

    Update and rename ContentView.swift to RegistrationView.swift

---
+++ RegistrationView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var name = ""
+    @State private var email = ""
+    @State private var phone = ""
+    @State private var instagram = ""
+    @State private var isEmailSelected = false
+    @State private var isPhoneSelected = false
+    @State private var isInstagramSelected = false
+    
+    var body: some View {
+        VStack {
+            TextField("Имя", text: $name)
+                .padding()
+            if isEmailSelected {
+                TextField("Email", text: $email)
+                    .padding()
+            } else if isPhoneSelected {
+                TextField("Телефон", text: $phone)
+                    .padding()
+            } else if isInstagramSelected {
+                TextField("Instagram", text: $instagram)
+                    .padding()
+            }
+            Button(action: {
+                // Регистрация логика
+            }) {
+                Text("Зарегистрироваться")
+            }
+        }
+    }
+}
