d69b3f581a9dc4458f30edb5129a645c8f67d7fc
skrypka-vladyslav <33196772+skrypka-vladyslav@users.noreply.github.com>
2025-02-07 19:05:03 +0200

    Update DateTimeSelectionView.swift

--- DateTimeSelectionView.swift
+++ DateTimeSelectionView.swift
@@ -1,20 +1,28 @@
 import SwiftUI
 
+@available(macOS 10.15, iOS 13.0, *)
 struct DateTimeSelectionView: View {
     @State private var selectedDate = Date()
     @State private var selectedTime = "10:00"
     
-    let availableTimes = ["10:00", "11:00", "12:00", ...]
+    let availableTimes = ["10:00", "11:00", "12:00"]
     
     var body: some View {
         VStack {
-            DatePicker("Выберите дату", selection: $selectedDate, in: Date()..., displayedComponents: .date)
-                .padding()
+            if #available(macOS 10.15, iOS 13.0, *) {
+                DatePicker("Выберите дату", selection: $selectedDate, in: Date()..., displayedComponents: .date)
+                    .padding()
+            } else {
+                Text("DatePicker не поддерживается на этой версии операционной системы.")
+            }
+            
             Picker("Выберите время", selection: $selectedTime) {
                 ForEach(availableTimes, id: \.self) { time in
                     Text(time)
                 }
             }
+            .padding()
+            
             Button(action: {
                 // Логика записи
             }) {
