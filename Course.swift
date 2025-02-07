0b6d8d6e472517eff8c1f9872582f4e0193c0c40
skrypka-vladyslav <33196772+skrypka-vladyslav@users.noreply.github.com>
2025-02-07 17:57:57 +0200

    Update Course.swift

--- Course.swift
+++ Course.swift
@@ -1,9 +1,7 @@
+import Foundation
 
-import SwiftUI
-
-struct Course: Identifiable, Codable {
-    var id = UUID()
+struct Course: Identifiable {
+    var id: UUID
     var name: String
-    var description: String
-    var imageName: String
+    var price: Double
 }
