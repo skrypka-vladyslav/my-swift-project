e4b4e36d6112cb2629d8c2ef4db89006651cb4a3
skrypka-vladyslav <33196772+skrypka-vladyslav@users.noreply.github.com>
2025-02-07 19:27:38 +0200

    Update MainView.swift

--- MainView.swift
+++ MainView.swift
@@ -2,6 +2,13 @@ import SwiftUI
 
 @available(macOS 10.15, iOS 13.0, *)
 struct MainView: View {
+    // Пример списка курсов
+    let sampleCourses = [
+        Course(name: "Курс 1", price: 100),
+        Course(name: "Курс 2", price: 150),
+        Course(name: "Курс 3", price: 200)
+    ]
+    
     var body: some View {
         TabView {
             CourseSelectionView(courses: sampleCourses)
