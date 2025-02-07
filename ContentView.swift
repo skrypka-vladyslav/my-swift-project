52ca307c3b8df44b6c51ba883f7851d1487a1bcb
skrypka-vladyslav <33196772+skrypka-vladyslav@users.noreply.github.com>
2025-02-07 19:18:43 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,13 +1,14 @@
 import SwiftUI
 
 @main
+@available(macOS 11.0, *)  // Указываем, что приложение поддерживает только macOS 11.0 и выше
 struct MyManicureApp: App {
     var body: some Scene {
         WindowGroup {
-            if #available(macOS 10.15, *) {  // Проверяем доступность macOS 10.15 или выше
+            if #available(macOS 11.0, *) {  // Проверка доступности macOS 11.0 или выше
                 MainView()
             } else {
-                Text("Требуется macOS 10.15 или новее для использования приложения")
+                Text("Требуется macOS 11.0 или новее для использования приложения")
             }
         }
     }
